80f9a094e935d8fa36c7d0b474df7d60b8061a7f
Florian Weber <florian.weber.dd@icloud.com>
2013-12-17 11:44:12 +0100

    ember app

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -1,6 +1,39 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
+window.App = Ember.Application.create();
 
+window.App.Router.map(function() {
+    this.route("about", {path: "/about"});
+    this.route("references", {path: "/references"});
+    this.route("contact", {path: "/contact"});
+});
+
+window.App.ApplicationController = Ember.Controller.extend({
+    updateLinks: function(path) {
+        $('.nav-link').livequery(function() {
+            $(this).removeClass('active-link');
+        });
+        $('.link-text').livequery(function() {
+            $(this).removeClass('active-text');
+        });
+        $('#al-' + path).livequery(function() {
+            $(this).addClass('active-link');
+        });
+        $('#at-' + path).livequery(function() {
+            $(this).addClass('active-text');
+        });
+    },
+    routeChanged: function() {
+        this.updateLinks(this.get('currentPath'));
+    }.observes('currentPath')
+});
+
+App.IndexRoute = Ember.Route.extend({
+    beforeModel: function() {
+        this.transitionTo('about');
+    }
+});
+
+App.AboutRoute = Ember.Route.extend({
+    setupController: function(controller) {
+    }
+});
